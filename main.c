500424630deb523968811e6f3de69988ab832cf3
Tetsuya HAMAMOTO <temmings@gmail.com>
2016-05-28 23:15:04 +0900

    Dump with formatted the date.

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "libpafe/libpafe.h"
 
 #define SUICA_SERVICE_CODE 0x090f
 #define SUICA_HISTORY_MAX 20
+
+struct tm* parse_suica_datetime(uint16 value) {
+    static struct tm t;
+    t.tm_year = 100 + (value >> 9);
+    t.tm_mon = ((value >> 5) & 0b1111) - 1;
+    t.tm_mday = value & 0b11111;
+    return &t;
+}
+
+char* format_suica_datetime(uint16 value) {
+    static char date[11];
+    strftime(date, sizeof(date), "%Y-%m-%d", parse_suica_datetime(value));
+    return date;
+}
 
 void dump_suica(felica* f) {
     uint8 b[FELICA_BLOCK_LENGTH];
@@ -14,7 +29,7 @@ void dump_suica(felica* f) {
         printf(" dev:%3d", b[0]);
         printf(" mode:%3d", b[1]);
         printf(" ??:0x%02x%02x", b[2], b[3]);
-        printf(" date:0x%02x%02x", b[4], b[5]);
+        printf(" date:%s", format_suica_datetime((b[4]<<8)+b[5]));
         printf(" from:0x%02x%02x", b[6],b[7]);
         printf(" to:0x%02x%02x", b[8], b[9]);
         printf(" remain:%5d", b[10] + (b[11]<<8));
